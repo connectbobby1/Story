29e06885284d21f958eb7a592011938d7e9f549c
Bobby <bobby.chodagam@yellowfin.bi>
2019-12-10 11:28:15 +1100

    updated LoginPageTest.java

--- src/test/java/Yellowfin/bi/qa/testcases/LoginPageTest.java
+++ src/test/java/Yellowfin/bi/qa/testcases/LoginPageTest.java
@@ -3,36 +3,33 @@ package Yellowfin.bi.qa.testcases;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 import Yellowfin.bi.qa.base.TestBase;
 import Yellowfin.bi.qa.pages.LicensePage;
 import Yellowfin.bi.qa.pages.LoginPage;
 
-
-
-public class LoginPageTest extends TestBase{
+public class LoginPageTest extends TestBase {
 	LoginPage loginPage;
 	LicensePage licensePage;
-	
-	public LoginPageTest(){
+
+	public LoginPageTest() {
 		super();
 	}
-	
+
 	@BeforeMethod
-	public void setUp(){
+	public void setUp() {
 		initialization();
-		loginPage = new LoginPage();	
+		loginPage = new LoginPage();
 	}
-	
-		
-	@Test(priority=1)
-	public void loginTest(){
+
+	@Test(priority = 1)
+	public void loginTest() {
 		licensePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
 	}
-	
-		
+
 	@AfterMethod
-	public void tearDown(){
-		//driver.quit();
+	public void tearDown() {
+		driver.quit();
 	}
-	
+
 }
